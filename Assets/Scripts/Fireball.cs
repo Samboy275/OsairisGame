011cd0d53e1d05ea8f6a26c25e88025bf5326871
Samual mohamed <62105186+Samboy275@users.noreply.github.com>
2021-01-29 15:30:48 +0200

    uploading

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+
+    public float fireSpeed = 20;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector2.right * fireSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject fireball;
+    public Transform fireposition;
+    public Animator anim;
+
+    private bool inputEnable = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputEnable)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                StartCoroutine(ShootFire());
+            }
+        }
+     
+    }
+
+
+    IEnumerator ShootFire()
+    {
+        inputEnable = false;
+        anim.SetBool("isFire", true);
+        yield return new WaitForSeconds(0.7f);
+        Instantiate(fireball, fireposition.transform.position, fireposition.rotation);
+        anim.SetBool("isFire", false);
+        inputEnable = true;
+    }
+}
