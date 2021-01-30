46331226befd8c305974d13e1e9480d68e9b1d12
Samual mohamed <62105186+Samboy275@users.noreply.github.com>
2021-01-30 17:43:33 +0200

    Trap Scene Lost :(

---
+++ Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed = 5.0f;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+        rb2d.velocity = transform.right * speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "SolidPlatform")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
